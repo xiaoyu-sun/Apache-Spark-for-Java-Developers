46fb70d417105f34a3d13063252ec9318649153d
Mason <44033871+xiaoyu-sun@users.noreply.github.com>
2020-02-17 14:16:21 -0600

    Create Main.java

---
+++ Demonstration Code/sec09/Main.java
@@ -0,0 +1,50 @@
+package com.virtualpairprogrammers;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Logger.getLogger("org.apache").setLevel(Level.WARN);
+
+        SparkConf conf = new SparkConf().setAppName("startingSpark").setMaster("local[*]");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<String> initialRdd = sc.textFile("src/main/resources/subtitles/input.txt");
+
+        // remove anything that is not a letter or whitespace
+        JavaRDD<String> lettersOnlyRdd = initialRdd.map( sentence -> sentence.replaceAll("[^a-zA-Z\\s]", "").toLowerCase() );
+
+        JavaRDD<String> removedBlankLines = lettersOnlyRdd.filter( sentence -> sentence.trim().length() > 0 );
+
+        JavaRDD<String> justWords = removedBlankLines.flatMap( sentence -> Arrays.asList(sentence.split(" ")).iterator());
+
+        JavaRDD<String> blankWordsRemoved = justWords.filter( word -> word.trim().length() > 0 );
+
+        JavaRDD<String> justInterestingWords = blankWordsRemoved.filter( word -> Util.isNotBoring(word) );
+
+        JavaPairRDD<String, Long> pairRdd = justInterestingWords.mapToPair( word -> new Tuple2<>(word, 1L) );
+
+        JavaPairRDD<String, Long> totals = pairRdd.reduceByKey( (value1, value2) -> value1 + value2 );
+
+        JavaPairRDD<Long, String> switched = totals.mapToPair( tuple -> new Tuple2<>(tuple._2, tuple._1) );
+
+        JavaPairRDD<Long, String> sorted = switched.sortByKey(false);
+
+        List<Tuple2<Long, String>> results = sorted.take(20);
+        results.forEach(System.out::println);
+
+        sc.close();
+
+    }
+}
